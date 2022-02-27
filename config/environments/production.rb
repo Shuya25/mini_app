e32640732dfd0ff471e1cb62fec69fa638b9cbdc
Shuya <buridaikon2481@gmail.com>
2022-02-27 06:00:27 +0000

    correct host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'safe-sands-78307.herokuapp.com'
+  host = 'https://safe-sands-78307.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
